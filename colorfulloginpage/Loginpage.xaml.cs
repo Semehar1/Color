37f27280ff5187158c77005a2a29f80c96bb12e3
Semehar Mebrahtu Hailu <semeharhailu@gmail.com>
2019-10-21 05:14:26 -0700

    first commit

---
+++ colorfulloginpage/Loginpage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace colorfulloginpage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Loginpage : ContentPage
+    {
+        public Loginpage()
+        {
+            InitializeComponent();
+        }
+        private void submit_Clicked(object sender, EventArgs e)
+        {
+            string name = username.Text;
+            string pass = password.Text;
+
+            DisplayAlert("Entries value", name + " " + pass, "ok");
+        }
+    }
+}
